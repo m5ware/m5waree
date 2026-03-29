7d54d14e69d0ea9067e9a8c3d2b529608440305d
pavel (m5ware) <220674268+m5ware@users.noreply.github.com>
2026-03-29 23:00:53 +0000

    Update loader2.lua to load loader3.lua

--- loader2.lua
+++ loader2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/m5ware/main/refs/heads/main/loader2.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/m5ware/main/refs/heads/main/loader3.lua'))()
